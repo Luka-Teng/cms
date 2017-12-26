bbf8a8db64231dd19b83245147c81ff46728bdbb
luka <359593891@qq.com>
2017-12-26 15:26:57 +0800

    'luka'

--- header-admin.php
+++ header-admin.php
@@ -14,7 +14,7 @@
 		<nav class="navbar navbar-inverse border-radius-0" role="navigation">
 			<div class="container-fluid">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+					<a class="navbar-brand" href="<?php echo home_url(); ?>">CMS</a>
 				</div>
 				<div>
 					<ul class="nav navbar-nav pull-right">
