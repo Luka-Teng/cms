74d069146e4a696e510efaf008b05b95eed80618
luka <359593891@qq.com>
2018-01-18 11:03:30 +0800

    'luka'

--- functions.php
+++ functions.php
@@ -597,14 +597,20 @@
 	}
 	function alipay_notifyUrl($request){
 		require_once("utils/payment/alipay/AopSdk.php");
+		$str = '';
+		foreach ($_POST as $key => $value) {
+			$str .= $key . '=' . $value . '&';
+		}
 		$aop = new AopClient;
 		$aop->alipayrsaPublicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv7PkFfpn7CRPVaHDtDz1FERp10hEMjjztYsfmiLLTRBGPAi6pIDTszwRSnkiB39osnwyaWeCJmGPMbp0qz6GgkqT/a+GTkbHnZA7vwykjfT8vA5i2EnqycfBem585o0dABZQfZEenIGGkxxcIK8gGe+d1tFnWjqR+T70uA/WrvoCtKDZ9oAWlPoUXCx9iPyunwPvFedurgMZZ3UiP3AE/avU0eAoWqYMrjXOZYELjdPLvwRRCn3n3Y9hzLejidCcXSwp0vkgtslL/t4w2hNOsidHHx/VGo6/OeTJeNGoqA3FtQMB8nPBUPnMMU5pf7XkulBCE1n0UbBaCLL3mvWsnQIDAQAB';
 		$flag = $aop->rsaCheckV1($_POST, 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv7PkFfpn7CRPVaHDtDz1FERp10hEMjjztYsfmiLLTRBGPAi6pIDTszwRSnkiB39osnwyaWeCJmGPMbp0qz6GgkqT/a+GTkbHnZA7vwykjfT8vA5i2EnqycfBem585o0dABZQfZEenIGGkxxcIK8gGe+d1tFnWjqR+T70uA/WrvoCtKDZ9oAWlPoUXCx9iPyunwPvFedurgMZZ3UiP3AE/avU0eAoWqYMrjXOZYELjdPLvwRRCn3n3Y9hzLejidCcXSwp0vkgtslL/t4w2hNOsidHHx/VGo6/OeTJeNGoqA3FtQMB8nPBUPnMMU5pf7XkulBCE1n0UbBaCLL3mvWsnQIDAQAB', 'utf-8', "RSA2");
 		write_log_file("lialialai");
 		if ($flag) {
 			write_log_file("ok");
+			write_log_file(str);
 		} else {
 			write_log_file("not ok");
+			write_log_file(str);
 		}
 	}
 	
