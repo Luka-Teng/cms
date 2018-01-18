6ec20465a06a5faedea8a4f2278c7dd7a6d978da
luka <359593891@qq.com>
2018-01-18 15:07:57 +0800

    'luka'

--- page-return-url.php
+++ page-return-url.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="alert alert-success">
-���ѳɹ�������ע���ʼ����գ���û�յ��ʼ�������ϵ�ͷ���
+<div class="alert alert-success top-gap-1">
+感谢您的购买，请注意邮箱的查收，如果没查收到邮件，请及时联系客服。
 </div>
 <?php get_footer(); ?>
