fce2afae0781e423ca0604b1c956884a712f94f2
luka <359593891@qq.com>
2018-01-18 14:28:58 +0800

    'luka'

--- functions.php
+++ functions.php
@@ -606,7 +606,8 @@
 	}
 	function alipay_notifyUrl($request){
 		#支付宝请求
-		require_once("utils/payment/payment.php");						
+		include("utils/payment/payment.php");
+		include("utils/payment/config.php");		
 		#创建alipay实例
 		$alipay = new Alipayment();
 		#用于输出日志
@@ -632,9 +633,25 @@
 			#验签成功，开始业务验证
 			if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
 				#判断是否存在该uid用户, 总金额是否正确，appid是否一致
-				require_once("utils/payment/config.php");
 				global $wpdb;
 				$applicant = $wpdb->get_row( "SELECT * FROM " . APPLICANT_TABLE . " WHERE uid = '{$_POST['out_trade_no']}' ", OBJECT );
+				if ($applicant) {
+					write_log_file("查到这个用户");
+				} else {
+					write_log_file("查不到这个用户");
+				}
+				if ($applicant->total_amount === $_POST['total_amount']) {
+					write_log_file("金额对");
+				} else {
+					write_log_file("金额不对");
+				}
+				if ($alipayConfig['appId'] === $_POST['app_id']) {
+					write_log_file("id in config : " . $alipayConfig['appId']);
+					write_log_file("id in params : " . $_POST['app_id']);
+				} else {
+					write_log_file("id in config : " . $alipayConfig['appId']);
+					write_log_file("id in params : " . $_POST['app_id']);
+				}
 				if ($applicant && $applicant->total_amount === $_POST['total_amount'] && $alipayConfig['appId'] === $_POST['app_id']) {
 					#实现业务逻辑
 					$query = $wpdb->insert( 
